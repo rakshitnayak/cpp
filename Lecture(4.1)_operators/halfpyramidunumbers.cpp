8e3fadb18fe7e64f9ba2d9bfc5f702d497801c93
rakshit <rdsonnn@gmail.com>
2020-10-30 19:26:28 +0530

    more patterns problems

---
+++ Lecture(4.1)_operators/halfpyramidunumbers.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+   
+    // 1
+    // 22
+    // 333
+    // output
+
+    // for(int i=1; i<=n; i++){
+    //     for (int j=1;j<=i;j++){
+    //          cout<<i<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+     
+    // flyods triangle
+    int count=1;
+    for(int i=1; i<=n; i++){
+        for (int j=1;j<=i;j++){
+             cout<<count<<" ";
+             count++;
+        }
+        cout<<endl;
+    }
+    
+
+
+
+}
