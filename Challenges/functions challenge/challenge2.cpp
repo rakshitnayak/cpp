adfedf928ca1c6c723fda132343a636e2d43a8a9
rakshit <rdsonnn@gmail.com>
2020-12-08 20:01:04 +0530

    functions challenge problems

---
+++ Challenges/functions challenge/challenge2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+//pythagoran triplet
+bool check(int x, int y, int z)
+{
+    int a = max(x, max(y, z));
+    int b, c;
+
+    if (a == x)
+    {
+        b = y;
+        c = z;
+    }
+    else if (a == y)
+    {
+        b = x;
+        c = z;
+    }
+    else
+    {
+        b = x;
+        c = y;
+    }
+
+    if (a * a == b * b + c * c)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int x, y, z;
+    cin >> x >> y >> z;
+
+    if (check(x, y, z))
+    {
+        cout << "Pythogrean triplet";
+    }
+    else
+    {
+        cout << "Not a pythagorean trilet";
+    }
+
+    return 0;
+}
---
+++ Challenges/functions challenge/challenge4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//octal to decimal
+int octaltoDecimal(int n)
+{
+
+    int num = n;
+    int dec_value = 0;
+
+    int base = 1;
+    int temp = num;
+    while (temp)
+    {
+        int last_digit = temp % 10;
+        temp = temp / 10;
+
+        dec_value += last_digit * base;
+        base = base * 8;
+    }
+    return dec_value;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    cout << octaltoDecimal(num) << endl;
+    return 0;
+}
