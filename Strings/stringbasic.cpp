df2cf44b1d78f372215e3b68ed75290382019e37
rakshit <rdsonnn@gmail.com>
2020-12-11 23:50:29 +0530

    string basics

---
+++ Strings/stringbasic.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s1 = "Rakshit"; //declare
+    //getline(cin,str)//input
+    s1.insert(2, "2");
+    cout << s1 << endl; //output
+    return 0;
+}
+
+//learnt all the functinons
+
+//append
+// clear
+// compare
+// empty
+// erase
+// find
+// insert
+// size/length
+// substring
+// stoi(string to integer)
+// to_string
