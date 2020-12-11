c792acd548b22f113255651633b35a9b048ad90a
rakshit <rdsonnn@gmail.com>
2020-12-12 00:01:35 +0530

    sorting string

---
+++ Strings/sortingString.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s1 = "yugggvbihzbygatfiszfxybyubh";
+
+    sort(s1.begin(), s1.end());
+
+    cout << s1 << endl;
+    return 0;
+}
