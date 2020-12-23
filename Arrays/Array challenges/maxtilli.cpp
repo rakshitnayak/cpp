34832fa67fae538ac2c6c05b199e8a794770d08c
rakshit <rdsonnn@gmail.com>
2020-12-23 13:03:34 +0530

    array challenge 1

---
+++ Arrays/Array challenges/maxtilli.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int mx = -1999999;
+    int n;
+
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        mx = max(mx, arr[i]);
+        cout << mx << endl;
+    }
+    cout << "maximum number in array is " << endl;
+    cout << mx << endl;
+    return 0;
+}
