a26c36bcdfd91525619e0fa85b410eba90c18e12
rakshit <rdsonnn@gmail.com>
2020-12-29 19:30:04 +0530

    dutch national flag algo

--- 450 DSA/ARRAY/012sorting.cpp
+++ 450 DSA/ARRAY/012sorting.cpp
@@ -1,3 +1,46 @@
+/* the main approach is using dutch national flag algorithm ,in this 
+ we will be having three  namely low ,mid and high!  */
+/* low=0,mid=0,high=n-1.., now we need to move mid untill it crosses high,
+it should satisfy 2 conditons after it crosses high.
+1) 0-low elements should be zero
+2) high+1 elements shoulld be 2
+*/
+/*  and should check the below 3 conditions while you move
+1)when mid==0, swap mid and low elements also increase their position by 1( i.e,low++ & mid++)
+2) when mid==1, only increase mid by 1 position (mid++)
+3) when mid==2,swap mid and high and decrease the high position by 1(i.e, high--)
+hurray!!! now you will get sorted array of 0's ,1's, and 2's
+*/
+//c++  program below
+/*
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                swap(nums[low++], nums[mid++]);
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[high--]);
+            }
+        }
+    }
+};
+*/
+
 // C++ implementation of the approach
 #include <bits/stdc++.h>
 using namespace std;
@@ -70,5 +113,5 @@ int main()
     return 0;
 }
 
-/*The first approach that comes in mind is to simply count the number of 0’s, 1’s, and 2’s. 
+/*The above approach is  to simply count the number of 0’s, 1’s, and 2’s. 
 Then, place all 0’s at the beginning of the array followed by 1’s and 2's.*/
