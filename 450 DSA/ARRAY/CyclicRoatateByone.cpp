487e5debf5e4797d0bcd574613144148e4f07d13
rakshit <rdsonnn@gmail.com>
2021-01-05 00:22:56 +0530

    understanding right shift

--- 450 DSA/ARRAY/CyclicRoatateByone.cpp
+++ 450 DSA/ARRAY/CyclicRoatateByone.cpp
@@ -9,24 +9,25 @@ main()
     {
         cin >> arr[i];
     }
-    //------------------left shift ---------------
-    // int temp=arr[n-1];
-    // for ( i = n-1 ; i > 0 ; i--)
-    // {
-    //     arr[i]=arr[i-1];
-
-    // }
-    // arr[0]=temp;
 
     //--------------------right shift------------
-    int temp = arr[0];
-    for (i = 0; i < n; i++)
+    // int temp = arr[0];
+    // for (i = 0; i < n; i++)
+    // {
+    //     arr[i] = arr[i + 1];
+    // }
+    // arr[n - 1] = temp;  /// here output for 1,2,3,4,5 will be 2,3,4,5,1
+
+    //------------------left shift ---------------
+    int temp = arr[n - 1];
+    for (i = n - 1; i > 0; i--)
     {
-        arr[i] = arr[i + 1];
+        arr[i] = arr[i - 1];
     }
-    arr[n - 1] = temp;
+    arr[0] = temp;
+
     for (i = 0; i < n; i++)
     {
-        cout << arr[i];
+        cout << arr[i] << " ";
     }
 }
