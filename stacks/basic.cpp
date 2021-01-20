a8a0f80eaefdb4fb95a32bf218fd609a295e41b2
rakshit <rdsonnn@gmail.com>
2021-01-20 23:55:08 +0530

    Stacks basics

---
+++ stacks/basic.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Stack
+{
+private:
+    int top;
+    int arr[5];
+
+public:
+    Stack()
+    {
+        top = -1;
+        for (int i = 0; i < 5; i++)
+        {
+            arr[i] = 0;
+        }
+    }
+
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool isFull()
+    {
+        if (top == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void push(int val)
+    {
+        if (isFull())
+        {
+            cout << "Stack overflow" << endl;
+        }
+        else
+        {
+            top++;
+            arr[top] = val;
+        }
+    }
+};
+
+int main()
+{
+}
