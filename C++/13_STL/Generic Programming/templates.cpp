529d91d97e555c13afcdaabff6fb63469ff2c279
rakshit <rdsonnn@gmail.com>
2021-04-13 22:03:58 +0530

    add stl initial

--- C++/13_STL/Generic Programming/templates.cpp
+++ C++/13_STL/Generic Programming/templates.cpp
@@ -1,8 +1,11 @@
+//all the algorithms and containers provided by stl are generic
+//generic means they can work with different data types.
+
 #include <iostream>
 
 using namespace std;
 
-template <typename T> //<class T> |both are fine
+template <typename T> //<class T> |both are fine Note:T is a placeholder when function is called of any datatype it will be given to T
 int search(T a[], int n, T key)
 {
     for (int i = 0; i < n; i++)
@@ -12,7 +15,7 @@ int search(T a[], int n, T key)
             return i;
         }
     }
-    return 0;
+    return -1;
 }
 
 int main()
@@ -20,10 +23,24 @@ int main()
 
     int a[] = {10, 20, 30, 40, 50};
     int n = sizeof(a) / sizeof(int);
-    // int key = 30;
+    //int key=30;
 
     float b[] = {1.1, 1.2, 1.3, 1.4};
     // float key = 1.2;
 
-    cout << search(b, n, key);
+    char arr2[] = {'a', 'b', 'c', 'd'};
+    char key = 'c';
+
+    cout << search(arr2, n, key);
 }
+
+/* some notes
+Templates are powerful features of C++ which allows you to write generic programs. In simple terms, 
+you can create a single function or a class to work with different data types using templates.
+
+Templates are often used in larger codebase for the purpose of code reusability and flexibility of the programs.
+
+The concept of templates can be used in two different ways:
+
+-> Function Templates
+-> Class Templates */
